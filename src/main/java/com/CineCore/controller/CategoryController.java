8668b0a91d2d6ea5666089af3003083e24b5ce52
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-24 16:12:19 -0300

    feat: Implementa os demais endpoints de busca por Categoria por ID, deleção e criação de novas categorias

--- src/main/java/com/CineCore/controller/CategoryController.java
+++ src/main/java/com/CineCore/controller/CategoryController.java
@@ -3,11 +3,10 @@ package com.CineCore.controller;
 import com.CineCore.entity.Category;
 import com.CineCore.service.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cinecore/category")
@@ -15,8 +14,29 @@ import java.util.List;
 public class CategoryController {
     private final CategoryService categoryService;
 
-    @GetMapping()
-    public List<Category> getAllCategories(){
+    @GetMapping
+    public List<Category> getAllCategories() {
         return categoryService.getAllCategories();
     }
+
+    @PostMapping
+    public Category saveCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById(@PathVariable Long id) {
+        Optional<Category> categoryFounded = categoryService.getCategoryById(id);
+        return categoryFounded.orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCategoryById(@PathVariable Long id){
+        if(getCategoryById(id) != null){
+            categoryService.deleteCategoryById(id);
+            return "Categoria de id " + id + " deletada com sucesso.";
+        }
+        return "Não foi possível encontrar uma categoria com o id " + id + ".";
+    }
+
 }
--- src/main/java/com/CineCore/service/CategoryService.java
+++ src/main/java/com/CineCore/service/CategoryService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +15,17 @@ public class CategoryService {
 
     public List<Category> getAllCategories(){
         return categoryRepository.findAll();
+    }
+
+    public Category saveCategory(Category category){
+        return categoryRepository.save(category);
+    }
+
+    public Optional<Category> getCategoryById(Long id){
+        return categoryRepository.findById(id);
+    }
+
+    public void deleteCategoryById(Long id){
+        categoryRepository.deleteById(id);
     }
 }
