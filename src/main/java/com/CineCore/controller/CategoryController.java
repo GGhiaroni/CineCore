a7ef3476874e316be34b5180fb6a855f05dca5a5
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-24 18:18:23 -0300

    fix: Trabalha com os DTOs CategoryRequest e CategoryResponse, protegendo a aplicação ao parar de trabalhar diretamente com a entidade

--- src/main/java/com/CineCore/controller/CategoryController.java
+++ src/main/java/com/CineCore/controller/CategoryController.java
@@ -1,6 +1,9 @@
 package com.CineCore.controller;
 
 import com.CineCore.entity.Category;
+import com.CineCore.mapper.CategoryMapper;
+import com.CineCore.request.CategoryRequest;
+import com.CineCore.response.CategoryResponse;
 import com.CineCore.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,19 +18,27 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public List<Category> getAllCategories() {
-        return categoryService.getAllCategories();
+    public List<CategoryResponse> getAllCategories() {
+        List<Category> categories = categoryService.getAllCategories();
+        return categories.stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList();
     }
 
     @PostMapping
-    public Category saveCategory(@RequestBody Category category) {
-        return categoryService.saveCategory(category);
+    public CategoryResponse saveCategory(@RequestBody CategoryRequest category) {
+        Category categoryMapped = CategoryMapper.toCategory(category);
+        Category categorySaved = categoryService.saveCategory(categoryMapped);
+        return CategoryMapper.toCategoryResponse(categorySaved);
     }
 
     @GetMapping("/{id}")
-    public Category getCategoryById(@PathVariable Long id) {
+    public CategoryResponse getCategoryById(@PathVariable Long id) {
         Optional<Category> categoryFounded = categoryService.getCategoryById(id);
-        return categoryFounded.orElse(null);
+        if (categoryFounded.isPresent()){
+            return CategoryMapper.toCategoryResponse(categoryFounded.get());
+        }
+        return null;
     }
 
     @DeleteMapping("/{id}")
