409191b7364c428b3e404f923123852cacfaffa3
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-24 11:04:23 -0300

    feat: Implementa a estrutura inicial do projeto

---
+++ src/main/java/com/CineCore/CineCoreApplication.java
@@ -0,0 +1,13 @@
+package com.CineCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CineCore/CineCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CineCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
