6a2a421389703ffbdefa8c6b8205043f3d9eab46
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-25 17:52:19 -0300

    feat: Implementa os primeiros endpoints de cadastrar e retornar todas os filmes do banco de dados no MovieController e MovieService

--- src/main/java/com/CineCore/controller/MovieController.java
+++ src/main/java/com/CineCore/controller/MovieController.java
@@ -1,14 +1,45 @@
 package com.CineCore.controller;
 
+import com.CineCore.entity.Category;
+import com.CineCore.entity.Movie;
+import com.CineCore.mapper.CategoryMapper;
+import com.CineCore.mapper.MovieMapper;
+import com.CineCore.request.CategoryRequest;
+import com.CineCore.request.MovieRequest;
+import com.CineCore.response.CategoryResponse;
+import com.CineCore.response.MovieResponse;
 import com.CineCore.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cinecore/movie")
 @RequiredArgsConstructor
 public class MovieController {
     private final MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllMovies() {
+        List<Movie> movies = movieService.getAllMovies();
+        if(movies != null) {
+            List<MovieResponse> movieResponseList = movies.stream()
+                    .map(MovieMapper::toMovieResponse)
+                    .toList();
+            return ResponseEntity.ok(movieResponseList);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhuma categoria foi encontrada.");
+    }
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> saveMovie(@RequestBody MovieRequest movieRequest) {
+        Movie movieSaved = movieService.saveMovie(MovieMapper.toMovie(movieRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                MovieMapper.toMovieResponse(movieSaved)
+        );
+    }
 
 }
--- src/main/java/com/CineCore/service/MovieService.java
+++ src/main/java/com/CineCore/service/MovieService.java
@@ -1,11 +1,27 @@
 package com.CineCore.service;
 
+import com.CineCore.entity.Movie;
+import com.CineCore.entity.Streaming;
 import com.CineCore.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
     private final MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies(){
+        return movieRepository.findAll();
+    }
+
+    public Movie saveMovie(Movie movie) {
+        return movieRepository.save(movie);
+
+    }
+
+
+
 }
