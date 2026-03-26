7cabd25a551ce5573c6d2618fa20a8201279f61f
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-26 14:14:41 -0300

    feat: Implementa os endpoints de listar movie por id e alterar um movie já cadastrado no banco de dados

--- src/main/java/com/CineCore/controller/MovieController.java
+++ src/main/java/com/CineCore/controller/MovieController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cinecore/movie")
@@ -25,7 +26,7 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<?> getAllMovies() {
         List<Movie> movies = movieService.getAllMovies();
-        if(movies != null) {
+        if (movies != null) {
             List<MovieResponse> movieResponseList = movies.stream()
                     .map(MovieMapper::toMovieResponse)
                     .toList();
@@ -34,12 +35,27 @@ public class MovieController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhuma categoria foi encontrada.");
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getMovieById(@PathVariable Long id) {
+        Optional<Movie> movieFound = movieService.getMovieById(id);
+        return movieFound
+                .map(movie -> ResponseEntity.ok().body(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
     @PostMapping
     public ResponseEntity<MovieResponse> saveMovie(@RequestBody MovieRequest movieRequest) {
         Movie movieSaved = movieService.saveMovie(MovieMapper.toMovie(movieRequest));
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 MovieMapper.toMovieResponse(movieSaved)
         );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieResponse> updateMovie(@PathVariable Long id, @RequestBody MovieRequest movieRequest) {
+        return movieService.updateMovie(id, MovieMapper.toMovie(movieRequest))
+                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
--- src/main/java/com/CineCore/service/MovieService.java
+++ src/main/java/com/CineCore/service/MovieService.java
@@ -7,8 +7,10 @@ import com.CineCore.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,25 +19,23 @@ public class MovieService {
     private final CategoryService categoryService;
     private final StreamingService streamingService;
 
-    private List<Category> findCategories(List<Category> categories){
+    private List<Category> findCategories(List<Category> categories) {
         List<Category> categoriesFound = new ArrayList<>();
-        for(Category category : categories){
+        for (Category category : categories) {
             categoryService.getCategoryById(category.getId()).ifPresent(categoriesFound::add);
         }
         return categoriesFound;
     }
 
-    private List<Streaming> findStreamings(List<Streaming> streamings){
+    private List<Streaming> findStreamings(List<Streaming> streamings) {
         List<Streaming> streamingsFound = new ArrayList<>();
-        for(Streaming streaming : streamings){
+        for (Streaming streaming : streamings) {
             streamingService.getStreamingById(streaming.getId()).ifPresent(streamingsFound::add);
         }
         return streamingsFound;
     }
 
-
-
-    public List<Movie> getAllMovies(){
+    public List<Movie> getAllMovies() {
         return movieRepository.findAll();
     }
 
@@ -46,6 +46,32 @@ public class MovieService {
 
     }
 
+    public Optional<Movie> getMovieById(Long id) {
+        return movieRepository.findById(id);
+    }
 
+    public Optional<Movie> updateMovie(Long id, Movie movie) {
+        Optional<Movie> movieFound = movieRepository.findById(id);
+        if (movieFound.isPresent()) {
+            List<Category> categories = this.findCategories(movie.getCategories());
+            List<Streaming> streamings = this.findStreamings(movie.getStreamings());
 
+            Movie movieUpdated = movieFound.get();
+            movieUpdated.setTitle(movie.getTitle());
+            movieUpdated.setDescription(movie.getDescription());
+            movieUpdated.setReleaseDate(movie.getReleaseDate());
+            movieUpdated.setRating(movie.getRating());
+
+            movieUpdated.getCategories().clear();
+            movieUpdated.getStreamings().clear();
+
+            movieUpdated.getCategories().addAll(categories);
+            movieUpdated.getStreamings().addAll(streamings);
+
+            movieRepository.save(movieUpdated);
+
+            return Optional.of(movieUpdated);
+        }
+        return Optional.empty();
+    }
 }
