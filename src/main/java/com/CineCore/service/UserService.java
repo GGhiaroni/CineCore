779edc8bd110b79ce32b9b1490ec1acf0e5129d3
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-27 06:49:18 -0300

    feat: Implementa o UserService e UserRepository já com o método responsável por salvar um user no banco de dados

--- src/main/java/com/CineCore/service/UserService.java
+++ src/main/java/com/CineCore/service/UserService.java
@@ -1,4 +1,16 @@
 package com.CineCore.service;
 
+import com.CineCore.entity.User;
+import com.CineCore.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
 }
