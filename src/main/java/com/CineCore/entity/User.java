6bffeebf8f869f88a495e2c7164387d1b1ecd576
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-27 06:47:34 -0300

    feat: Cria a Entity user para começar a trabalhar com o registro de usuários na aplicação

--- src/main/java/com/CineCore/entity/User.java
+++ src/main/java/com/CineCore/entity/User.java
@@ -1,4 +1,26 @@
 package com.CineCore.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
