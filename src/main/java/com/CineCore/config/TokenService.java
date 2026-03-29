aaba2a1e2c1f1bb58c08c5b9b79b168af95ce64b
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-29 13:23:11 -0300

    feat: Implementa o método verifyToken, responsável por verificar se o token fornecido pelo usuário é legítimo e váldio

--- src/main/java/com/CineCore/config/TokenService.java
+++ src/main/java/com/CineCore/config/TokenService.java
@@ -3,10 +3,13 @@ package com.CineCore.config;
 import com.CineCore.entity.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -14,7 +17,7 @@ public class TokenService {
     @Value("${cinecore.security.secret}")
     private String secret;
 
-    public String generateToken(User user){
+    public String generateToken(User user) {
         Algorithm algorithm = Algorithm.HMAC256(secret);
 
         return JWT.create()
@@ -25,6 +28,25 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer("API Cinecore")
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> verifyToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            DecodedJWT jwt = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(jwt.getClaim("userId").asLong())
+                    .name(jwt.getClaim("name").asString())
+                    .email(jwt.getSubject())
+                    .build());
+        } catch (JWTVerificationException exception) {
+            return Optional.empty();
+        }
     }
 }
 
