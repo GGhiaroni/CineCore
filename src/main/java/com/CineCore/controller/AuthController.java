fef774395ec1fb020efc2bb9751efdc05d28a142
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-29 11:06:52 -0300

    feat: Implementa o endpoint responsável pelo login do usuário cadastrado, devolvendo o token de acesso em caso de sucesso

--- src/main/java/com/CineCore/controller/AuthController.java
+++ src/main/java/com/CineCore/controller/AuthController.java
@@ -1,13 +1,19 @@
 package com.CineCore.controller;
 
+import com.CineCore.config.TokenService;
 import com.CineCore.entity.User;
 import com.CineCore.mapper.UserMapper;
+import com.CineCore.request.LoginRequest;
 import com.CineCore.request.UserRequest;
+import com.CineCore.response.LoginResponse;
 import com.CineCore.response.UserResponse;
 import com.CineCore.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +24,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest userRequest){
         User savedUser = userService.saveUser(UserMapper.toUser(userRequest));
         return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse(savedUser));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
+        UsernamePasswordAuthenticationToken userAndPassword = new UsernamePasswordAuthenticationToken(
+                loginRequest.email(),
+                loginRequest.password()
+        );
+
+        Authentication authenticate = authenticationManager.authenticate(userAndPassword);
+
+        User user = (User) authenticate.getPrincipal();
+
+        String token = tokenService.generateToken(user);
+
+        return ResponseEntity.ok(new LoginResponse(token));
     }
 
 }
