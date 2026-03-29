c05b649337fc700e96f6b500f06611dcdc11125e
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-29 11:06:06 -0300

    feat: Utiliza o PasswordEncoder para criptografar a senha do usuário antes de salvar os seus dados no banco de dados no momento de um cadastro

--- src/main/java/com/CineCore/service/UserService.java
+++ src/main/java/com/CineCore/service/UserService.java
@@ -3,14 +3,18 @@ package com.CineCore.service;
 import com.CineCore.entity.User;
 import com.CineCore.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User saveUser(User user){
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
     }
 }
