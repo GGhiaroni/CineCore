dba8650e5e1a1912b9a0d9fcbfcaabec5d0035b6
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-30 22:34:08 -0300

    docs: Cria o arquivo de configuração do Swagger

---
+++ src/main/java/com/CineCore/config/SwaggerConfiguration.java
@@ -0,0 +1,29 @@
+package com.CineCore.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI getOpenApi(){
+
+        Contact contact = new Contact();
+        contact.setUrl("https://github.com/GGhiaroni");
+        contact.name("Gabriel Ghiaroni Tiziano");
+        contact.setEmail("gghiaronitiziano@gmail.com");
+
+        Info info = new Info();
+
+        info.title("CineCore");
+        info.version("v1");
+        info.description("Aplicação de gerenciamento de um catálogo de filmes");
+        info.contact(contact);
+
+        return new OpenAPI().info(info);
+    }
+}
